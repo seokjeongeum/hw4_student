2e16132c2d237de93b2d753fea91d58348f8fb55
root <jeseok@dblab.postech.ac.kr>
2025-03-07 15:28:40 +0900

    remove unused event files and __pycache__ directories; update execution counts in DQN notebook

--- deeprl/policies/MLP_policy.py
+++ deeprl/policies/MLP_policy.py
@@ -13,19 +13,19 @@ from deeprl.policies.base_policy import BasePolicy
 
 
 class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
-
-    def __init__(self,
-                 ac_dim,
-                 ob_dim,
-                 n_layers,
-                 size,
-                 discrete=False,
-                 learning_rate=1e-4,
-                 entropy_weight=0.,
-                 training=True,
-                 nn_baseline=False,
-                 **kwargs
-                 ):
+    def __init__(
+        self,
+        ac_dim,
+        ob_dim,
+        n_layers,
+        size,
+        discrete=False,
+        learning_rate=1e-4,
+        entropy_weight=0.0,
+        training=True,
+        nn_baseline=False,
+        **kwargs
+    ):
         super().__init__(**kwargs)
 
         # init vars
@@ -35,25 +35,29 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         self.discrete = discrete
         self.size = size
         self.learning_rate = learning_rate
-        self.entropy_weight = entropy_weight # only used for actor critic
+        self.entropy_weight = entropy_weight  # only used for actor critic
         self.training = training
         self.nn_baseline = nn_baseline
 
         if self.discrete:
-            self.logits_na = ptu.build_mlp(input_size=self.ob_dim,
-                                           output_size=self.ac_dim,
-                                           n_layers=self.n_layers,
-                                           size=self.size)
+            self.logits_na = ptu.build_mlp(
+                input_size=self.ob_dim,
+                output_size=self.ac_dim,
+                n_layers=self.n_layers,
+                size=self.size,
+            )
             self.logits_na.to(ptu.device)
             self.mean_net = None
             self.logstd = None
-            self.optimizer = optim.Adam(self.logits_na.parameters(),
-                                        self.learning_rate)
+            self.optimizer = optim.Adam(self.logits_na.parameters(), self.learning_rate)
         else:
             self.logits_na = None
-            self.mean_net = ptu.build_mlp(input_size=self.ob_dim,
-                                      output_size=self.ac_dim,
-                                      n_layers=self.n_layers, size=self.size)
+            self.mean_net = ptu.build_mlp(
+                input_size=self.ob_dim,
+                output_size=self.ac_dim,
+                n_layers=self.n_layers,
+                size=self.size,
+            )
             self.logstd = nn.Parameter(
                 torch.zeros(self.ac_dim, dtype=torch.float32, device=ptu.device)
             )
@@ -61,7 +65,7 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             self.logstd.to(ptu.device)
             self.optimizer = optim.Adam(
                 itertools.chain([self.logstd], self.mean_net.parameters()),
-                self.learning_rate
+                self.learning_rate,
             )
 
         if nn_baseline:
@@ -104,14 +108,14 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     # This function outputs a distribution object representing the policy output for the particular observations.
-    # assumes first dimension of observations 
+    # assumes first dimension of observations
     def forward(self, observation: torch.FloatTensor):
         if self.discrete:
             logits = self.logits_na(observation)
             return torch.distributions.Categorical(logits=logits)
         else:
             batch_mean = self.mean_net(observation)
-            logstd = torch.clamp(self.logstd, -10, 2) 
+            logstd = torch.clamp(self.logstd, -10, 2)
             scale_tril = torch.diag(torch.exp(logstd))
             batch_dim = batch_mean.shape[0]
             batch_scale_tril = scale_tril.repeat(batch_dim, 1, 1)
@@ -120,17 +124,16 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
                 scale_tril=batch_scale_tril,
             )
 
+
 #####################################################
 #####################################################
+
 
 class MLPPolicySL(MLPPolicy):
     def __init__(self, ac_dim, ob_dim, n_layers, size, **kwargs):
         super().__init__(ac_dim, ob_dim, n_layers, size, **kwargs)
 
-    def update(
-            self, observations, actions,
-            adv_n=None, acs_labels_na=None, qvals=None
-    ):
+    def update(self, observations, actions, adv_n=None, acs_labels_na=None, qvals=None):
         observations = ptu.from_numpy(observations)
         actions = ptu.from_numpy(actions)
         """
@@ -147,27 +150,25 @@ class MLPPolicySL(MLPPolicy):
         loss.backward()
         self.optimizer.step()
 
-
         return {
             # You can add extra logging information here, but keep this line
-            'Training Loss': ptu.to_numpy(loss),
+            "Training Loss": ptu.to_numpy(loss),
         }
+
 
 class MLPPolicyPG(MLPPolicy):
     def __init__(self, ac_dim, ob_dim, n_layers, size, **kwargs):
-
         super().__init__(ac_dim, ob_dim, n_layers, size, **kwargs)
         self.baseline_loss = nn.MSELoss()
 
-    def update(self, observations, acs_na, adv_n=None, acs_labels_na=None,
-                   qvals=None):
+    def update(self, observations, acs_na, adv_n=None, acs_labels_na=None, qvals=None):
         observations = ptu.from_numpy(observations)
         actions = ptu.from_numpy(acs_na)
         adv_n = ptu.from_numpy(adv_n)
         """
         TODO: compute the policy gradient given already compute advantages adv_n
         """
-        loss = None
+        loss = (-self(observations).log_prob(actions) * adv_n).mean()
         """
         END CODE
         """
@@ -183,7 +184,9 @@ class MLPPolicyPG(MLPPolicy):
             TODO: update the baseline value function by regressing to the values
             Hint: see self.baseline_loss for the appropriate loss
             """
-            baseline_loss = None
+            baseline_loss = self.baseline_loss(
+                self.baseline(observations).squeeze(-1), targets_n
+            )
             """
             END CODE
             """
@@ -193,21 +196,22 @@ class MLPPolicyPG(MLPPolicy):
         else:
             baseline_loss = None
         return {
-            'Training Loss': ptu.to_numpy(loss),
-            'Baseline Loss': ptu.to_numpy(baseline_loss) if baseline_loss else 0,
+            "Training Loss": ptu.to_numpy(loss),
+            "Baseline Loss": ptu.to_numpy(baseline_loss) if baseline_loss else 0,
         }
 
     def run_baseline_prediction(self, observations):
         """
-            Helper function that converts `observations` to a tensor,
-            calls the forward method of the baseline MLP,
-            and returns a np array
-            Input: `observations`: np.ndarray of size [N, 1]
-            Output: np.ndarray of size [N]
+        Helper function that converts `observations` to a tensor,
+        calls the forward method of the baseline MLP,
+        and returns a np array
+        Input: `observations`: np.ndarray of size [N, 1]
+        Output: np.ndarray of size [N]
         """
         observations = ptu.from_numpy(observations)
         pred = self.baseline(observations)
         return ptu.to_numpy(pred.squeeze())
+
 
 class MLPPolicyAC(MLPPolicy):
     def forward(self, observations: torch.FloatTensor):
@@ -216,12 +220,12 @@ class MLPPolicyAC(MLPPolicy):
         else:
             base_dist = super(MLPPolicyAC, self).forward(observations)
             # for AC methods, we need to ensure actions sampled from the env
-            # are valid actions for the environment. 
+            # are valid actions for the environment.
             # Since the action spaces are bounded between [-1, 1], we apply
             # a tanh function to the Gaussian policy samples.
             return torch.distributions.transformed_distribution.TransformedDistribution(
-                    base_dist, [torch.distributions.transforms.TanhTransform()])
-
+                base_dist, [torch.distributions.transforms.TanhTransform()]
+            )
 
     def update(self, observations, critic):
         observations = ptu.from_numpy(observations)
@@ -241,5 +245,5 @@ class MLPPolicyAC(MLPPolicy):
         loss.backward()
         self.optimizer.step()
         return {
-            'Actor Training Loss': ptu.to_numpy(loss),
+            "Actor Training Loss": ptu.to_numpy(loss),
         }
